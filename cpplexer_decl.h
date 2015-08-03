9f3c314bde2b661f6e52adaa341d7a53180af684
Stone Steps <support@stonesteps.ca>
2015-08-03 14:14:40 -0400

    * Added copyright notices to all source files * Added COPYING and Copyright files

--- cpplexer_decl.h
+++ cpplexer_decl.h
@@ -1,3 +1,10 @@
+/*
+    linecnt - a source line counting utility
+
+    Copyright (c) 2003-2015, Stone Steps Inc. (www.stonesteps.ca)
+
+    See COPYING and Copyright files for additional licensing and copyright information
+*/
 #ifndef __CPPLEXER_DECL_H
 #define __CPPLEXER_DECL_H
 
--- cpplexer_imp.cpp
+++ cpplexer_imp.cpp
@@ -1,3 +1,10 @@
+/*
+    linecnt - a source line counting utility
+
+    Copyright (c) 2003-2015, Stone Steps Inc. (www.stonesteps.ca)
+
+    See COPYING and Copyright files for additional licensing and copyright information
+*/
 #define __CPPLEXER_IMP_CPP
 
 //
